d8d655f7213fe1c421a74de1b15c0914c0b99a32
Sewer56 <sewer56lol@googlemail.com>
2025-01-26 12:54:23 +0000

    Added: Initial Commit to Project

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![doc = include_str!("../README.MD")]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
